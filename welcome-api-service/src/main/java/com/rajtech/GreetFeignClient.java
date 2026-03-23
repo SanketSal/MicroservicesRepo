ae11f1a06b069ac58e8eb1d698c4f297df7d938d
SanketSal <salankimatt.sanket@gmail.com>
2026-03-23 20:52:52 +0530

    Microservices proj

---
+++ welcome-api-service/src/main/java/com/rajtech/GreetFeignClient.java
@@ -0,0 +1,11 @@
+package com.rajtech;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name="greet-api")
+public interface GreetFeignClient {
+
+	@GetMapping("/greet")
+	public String invokeGreetApi();
+}
